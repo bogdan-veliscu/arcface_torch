f26fd3a867da9f6626cb2c9f9d5145e64d12dd5d
Bogdan Veliscu <bogdan.veliscu@outlook.com>
2021-04-13 15:37:38 +0300

    updated setup.py with the propper methadata

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 import setuptools
 
-requirements = ["numpy", "Pillow", "torch", "torchvision", "tqdn", "easydict", "opencv-python"]
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requirements = ["numpy",
+                "Pillow",
+                "torch",
+                "torchvision",
+                "tqdn",
+                "easydict",
+                "opencv-python"]
 
 setuptools.setup(
-     package_data={
+    name="arcface_torch",
+    version="0.1.0",
+    description="A PyTorch re-implementation of ArcFace",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomasic/arcface_torch",
+    package_data={
         "": ["*.txt", "*.rst", "*.pth", "*.npy", "*.log"],
     }
 )
